77e855ae8bb75b2446530885c4a3555b3e52a3f5
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-12-05 08:15:16 -0500

    Added FolderPath, GetFiles(), constructor

--- FilesOnDvdLocal/DvdFolderToImport.cs
+++ FilesOnDvdLocal/DvdFolderToImport.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilesOnDvdLocal {
     public class DvdFolderToImport {
+        public string FolderPath { get; set; }
         public List<FileToImport> Files { get; set; }
+
+        public DvdFolderToImport(string folderPath) {
+            FolderPath = folderPath;
+            Files = new List<FileToImport>();
+        }
+
+        public void GetFiles() {
+            if (Directory.Exists(FolderPath)) {
+                var files = Directory.GetFiles(FolderPath);
+                foreach (string file in files) {
+                    FileToImport fileToImport = new FileToImport(file);
+                    Files.Add(fileToImport);
+                }
+            }
+        }
     }
 }
