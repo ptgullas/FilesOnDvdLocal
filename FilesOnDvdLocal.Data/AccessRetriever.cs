0a15842fc89927198976e9660470d849c74f587e
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2019-12-05 15:59:27 -0500

    Added working code for Access DB Lookup. Retrieves DataSet for Series, Performers, Aliases

--- FilesOnDvdLocal.Data/AccessRetriever.cs
+++ FilesOnDvdLocal.Data/AccessRetriever.cs
@@ -5,36 +5,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
 using System.Data;
+using Serilog;
 
 namespace FilesOnDvdLocal.Data
 {
     public class AccessRetriever {
-        private OleDbConnection _dbConnection;
+        public string DatabasePath { get; set; }
 
-        public void OpenDbConnection() {
-            _dbConnection = new OleDbConnection {
-                ConnectionString = GetConnectionString()
-            };
+        public AccessRetriever(string databasePath) {
+            DatabasePath = databasePath;
         }
 
         private string GetConnectionString() {
-            string dbLocation = @"c:\temp\Files on Dvd.accdb";
-            return $"Provider=Microsoft.ACE.OLEDB.14.0;Data Source={dbLocation}";
+            return $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={DatabasePath}";
         }
 
-        public void CloseDbConnection() {
-            _dbConnection.Close();
+        public DataSet GetSeriesAndPerformersAndAliases() {
+            DataSet dataSet = new DataSet();
+            using (OleDbConnection connection = new OleDbConnection(GetConnectionString())) {
+                OleDbCommand selectSeriesAndPerformersCmd = new OleDbCommand("SELECT * FROM tblSeries", connection);
+                OleDbDataAdapter seriesAdapter = new OleDbDataAdapter {
+                    SelectCommand = selectSeriesAndPerformersCmd
+                };
+                //seriesAdapter.TableMappings.Add("Table", "Series");
+                //seriesAdapter.TableMappings.Add("Table1", "Performers");
+                seriesAdapter.Fill(dataSet, "Series");
+
+                OleDbCommand getPerformersCmd = new OleDbCommand("SELECT * FROM tblPerformers", connection);
+                OleDbDataAdapter performersAdapter = new OleDbDataAdapter {
+                    SelectCommand = getPerformersCmd
+                };
+                performersAdapter.Fill(dataSet, "Performers");
+
+                OleDbCommand getAliasesCmd = new OleDbCommand("SELECT * FROM tblPerformerAliases", connection);
+                OleDbDataAdapter aliasesAdapter = new OleDbDataAdapter {
+                    SelectCommand = getAliasesCmd
+                };
+                aliasesAdapter.Fill(dataSet, "Aliases");
+            }
+            return dataSet;
         }
 
-        public void GetSeries() {
-            string strSql = "SELECT * FROM tblSeries";
-            OpenDbConnection();
-            OleDbDataAdapter myCmd = new OleDbDataAdapter(strSql, _dbConnection);
-            _dbConnection.Open();
-            DataSet dtSet = new DataSet();
-            myCmd.Fill(dtSet, "tblSeries");
-            DataTable dataTable = dtSet.Tables[0];
-            CloseDbConnection();
+        public DataTable GetSeries() {
+            DataSet dataSet = new DataSet();
+            using (OleDbConnection connection = new OleDbConnection(GetConnectionString())) {
+                OleDbCommand getSeriesCmd = new OleDbCommand("SELECT * FROM tblSeries", connection);
+                OleDbDataAdapter seriesAdapter = new OleDbDataAdapter {
+                    SelectCommand = getSeriesCmd
+                };
+                try {
+                    seriesAdapter.Fill(dataSet);
+                }
+                catch (Exception e) {
+                    Log.Error(e, "Error retrieving Series");
+                }
+                DataTable seriesTable = dataSet.Tables[0];
+                return seriesTable;
+            }
+
         }
 
 
---
+++ FilesOnDvdLocal.Tests/AccessRetrieverTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using FilesOnDvdLocal.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests
+{
+    [TestClass]
+    public class AccessRetrieverTests
+    {
+        [TestMethod]
+        public void GetSeries_Temp_ActuallyGetsSeries() {
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            AccessRetriever myRetriever = new AccessRetriever(dbLocation);
+            DataTable dt = myRetriever.GetSeries();
+            string seriesToLookUp = "in the vIP";
+            DataRow[] selectedRows = dt.Select($"Series = '{seriesToLookUp}'");
+        }
+
+        [TestMethod]
+        public void GetSeriesAndPerformersAndAliases_Temp_ActuallyGetsSeries() {
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            AccessRetriever myRetriever = new AccessRetriever(dbLocation);
+            DataSet resultSet = myRetriever.GetSeriesAndPerformersAndAliases();
+            DataTable seriesTable = resultSet.Tables[0];
+            string seriesToLookUp = "doctor who podshock";
+            DataRow[] selectedSeriesRows = seriesTable.Select($"Series = '{seriesToLookUp}'");
+
+            DataTable performersTable = resultSet.Tables[1];
+            string performerToLookUp = "christine mendoza";
+            DataRow[] selectedPerformersRows = performersTable.Select($"Performer = '{performerToLookUp}'");
+
+            DataTable aliasTable = resultSet.Tables[2];
+            string aliasToLookUp = "lucky";
+            DataRow[] selectedAliasRows = aliasTable.Select($"Alias = '{aliasToLookUp}'");
+
+
+            Console.WriteLine("debug this line");
+        }
+    }
+}
