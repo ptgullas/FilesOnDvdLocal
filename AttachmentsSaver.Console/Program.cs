07b107238c5afcc2a92534ac740620ff2c45da39
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2019-12-06 16:48:42 -0500

    Added working code for Saving Screenshot & Headshot attachments from Access Database.

---
+++ AttachmentsSaver.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using FilesOnDvdLocal.Data;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachmentsSaver
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            SetUpLogging();
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            // RetrieveScreenshots(dbLocation);
+            RetrieveHeadshots(dbLocation);
+        }
+
+        private static void RetrieveScreenshots(string dbLocation) {
+            AccessAttachmentsRetriever retriever = new AccessAttachmentsRetriever(dbLocation);
+            retriever.GetScreenshots();
+        }
+
+        private static void RetrieveHeadshots(string dbLocation) {
+            AccessAttachmentsRetriever retriever = new AccessAttachmentsRetriever(dbLocation);
+            retriever.GetHeadshots();
+        }
+
+
+        private static void SetUpLogging() {
+            try {
+                //string logFolder = Configuration.GetSection("LocalFolders").GetValue<string>("logFolder");
+                //string logFile = Configuration.GetSection("LocalFolders").GetValue<string>("logFileName");
+                //string logPath = Path.Combine(logFolder, logFile);
+                string logPath = @"c:\Logs\FilesOnDvdLocal\FilesOnDvdLocalLog.txt";
+
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.File(logPath, rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Could not set up logging");
+                Console.WriteLine(e.GetBaseException());
+            }
+        }
+
+    }
+}
