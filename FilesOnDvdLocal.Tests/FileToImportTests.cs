4a01996f65dd06a5001c6b1b654dff8d9215a235
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-12-06 09:30:51 -0500

    Added GetSeriesNameFromFilename, GetPerformersFromFilename

--- FilesOnDvdLocal.Tests/FileToImportTests.cs
+++ FilesOnDvdLocal.Tests/FileToImportTests.cs
@@ -67,5 +67,65 @@ namespace FilesOnDvdLocal.Tests {
             Assert.AreEqual(expected, result.Count);
         }
 
+        [TestMethod]
+        public void GetSeriesNameFromFilename_NameIsNormal_ReturnsName() {
+            string filePath = @"C:\temp\Watchmen - Jean Smart, Jeremy Irons, Regina King - Little Fear of Lightning (2019-12-05).mkv";
+            string expected = "Watchmen";
+            FileToImport fileToImport = new FileToImport(filePath);
+
+            string result = fileToImport.GetSeriesNameFromFilename();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetPerformersFromFilename_NameHasCommas_ReturnsPerformers() {
+            string filePath = @"C:\temp\Watchmen - Jean Smart, Jeremy Irons, Regina King - Little Fear of Lightning (2019-12-05).mkv";
+            string expected = "Jeremy Irons";
+            FileToImport fileToImport = new FileToImport(filePath);
+
+            var result = fileToImport.GetPerformersFromFilename();
+            Assert.AreEqual(expected, result[1]);
+        }
+
+        [TestMethod]
+        public void GetPerformersFromFilename_NameHasAmpersand_ReturnsPerformers() {
+            string filePath = @"C:\temp\Watchmen - Trent Reznor & Atticus Ross - Little Fear of Lightning (2019-12-05).mkv";
+            string expected = "Atticus Ross";
+            FileToImport fileToImport = new FileToImport(filePath);
+
+            var result = fileToImport.GetPerformersFromFilename();
+            Assert.AreEqual(expected, result[1]);
+        }
+
+        [TestMethod]
+        public void GetPerformersFromFilename_NameHasAmpersandThenComma_ReturnsPerformers() {
+            string filePath = @"C:\temp\Succession - Logan Roy & Shiv Roy, Roman Roy - Little Fear of Lightning (2019-12-05).mkv";
+            string expected1 = "Logan Roy";
+            string expected2 = "Shiv Roy";
+            string expected3 = "Roman Roy";
+            FileToImport fileToImport = new FileToImport(filePath);
+
+            var result = fileToImport.GetPerformersFromFilename();
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(expected1, result[0]);
+            Assert.AreEqual(expected2, result[1]);
+            Assert.AreEqual(expected3, result[2]);
+        }
+
+        [TestMethod]
+        public void GetPerformersFromFilename_NameHasCommasThenAmpersand_ReturnsPerformers() {
+            string filePath = @"C:\temp\Arrow - Oliver Queen, Laurel Lance & Harbinger - Little Fear of Lightning (2019-12-05).mkv";
+            string expected1 = "Oliver Queen";
+            string expected2 = "Laurel Lance";
+            string expected3 = "Harbinger";
+            FileToImport fileToImport = new FileToImport(filePath);
+
+            var result = fileToImport.GetPerformersFromFilename();
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(expected1, result[0]);
+            Assert.AreEqual(expected2, result[1]);
+            Assert.AreEqual(expected3, result[2]);
+        }
+
     }
 }
--- FilesOnDvdLocal/FileToImport.cs
+++ FilesOnDvdLocal/FileToImport.cs
@@ -48,5 +48,58 @@ namespace FilesOnDvdLocal {
             }
             return positions;
         }
+
+        public string GetSeriesNameFromFilename() {
+            string seriesName = null;
+            string filename = File.Name;
+            int indexOfHyphen = filename.IndexOf("-");
+            if (indexOfHyphen >= 0) {
+                seriesName = filename.Substring(0, indexOfHyphen - 1).Trim();
+            }
+            return seriesName;
+        }
+
+        public List<string> GetPerformersFromFilename() {
+            List<string> performers = new List<string>();
+            string filename = File.Name;
+            int indexOfHyphen = filename.IndexOf("-");
+            if (indexOfHyphen >= 0) {
+                int indexOfSecondDelimeter = GetIndexOfNextDelimeter(filename, indexOfHyphen + 1);
+                if (indexOfSecondDelimeter >= 0) {
+                    string performersSubstring = filename.Substring(indexOfHyphen + 1, indexOfSecondDelimeter - indexOfHyphen - 1).Trim();
+                    List<string> tempPerformers = performersSubstring.Split(',').ToList();
+                    if (performersSubstring.Contains("&")) {
+                        foreach (string tempPerformersSplit in tempPerformers) {
+                            if (!tempPerformersSplit.Contains("&")) {
+                                performers.Add(tempPerformersSplit.Trim());
+                            }
+                            else {
+                                List<string> tempPerformersSplitAmpersand = tempPerformersSplit.Split('&').ToList();
+                                foreach (string performer in tempPerformersSplitAmpersand) {
+                                    performers.Add(performer.Trim());
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        foreach (string performer in tempPerformers) {
+                            performers.Add(performer.Trim());
+                        }
+                    }
+                }
+            }
+            return performers;
+        }
+
+
+
+        private static int GetIndexOfNextDelimeter(string filename, int start) {
+            int indexOfSecondDelimeter = filename.IndexOf("-", start);
+            if (indexOfSecondDelimeter < 0) {
+                indexOfSecondDelimeter = filename.IndexOf("(", start);
+            }
+
+            return indexOfSecondDelimeter;
+        }
     }
 }
