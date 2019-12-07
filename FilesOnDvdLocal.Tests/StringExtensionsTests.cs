2c1c8777f86e6ddbed382ee0ce6e8bbd7bf5e918
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-12-06 21:31:26 -0500

    Added StringExtensions & tests

---
+++ FilesOnDvdLocal.Tests/StringExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests {
+    [TestClass]
+    public class StringExtensionsTests {
+        [TestMethod]
+        public void GetIndexOfFirstHyphen_HasHyphenReturnFirstHyphen_ReturnsIndexOfFirstHyphen() {
+            string fileName = "Archer - 10.01 - 1999-Bort the Garj.mkv";
+            int expected = 7;
+            int result = fileName.GetIndexOfFirstHyphen();
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void GetIndexOfFirstHyphen_HasHyphenSkipFirstHyphen_ReturnsIndexOfSecondHyphen() {
+            string fileName = "Archer - 10.01 - 1999-Bort the Garj.mkv";
+            int expected = 7;
+            int result = fileName.GetIndexOfFirstHyphen(1);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ FilesOnDvdLocal/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal {
+    public static class StringExtensions {
+        public static int GetIndexOfFirstHyphen(this string str, int skip = 0) {
+            int index = str.IndexOf("-");
+            if (skip > 0 && index >= 0) {
+                int hyphenCount = str.Count(f => f == '-');
+                for (int i = 0; i <= hyphenCount; i++) {
+                    index = str.IndexOf("-", index + 1);
+                }
+            }
+            return index;
+        }
+    }
+}
