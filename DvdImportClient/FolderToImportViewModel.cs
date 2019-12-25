071e976b57c7be9cf65525a0d2a29b3806caec31
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-12-25 08:43:36 -0500

    Added FolderToImportView

--- DvdImportClient/FolderToImportViewModel.cs
+++ DvdImportClient/FolderToImportViewModel.cs
@@ -1,28 +1,42 @@
 ﻿using FilesOnDvdLocal;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DvdImportClient {
     public class FolderToImportViewModel : INotifyPropertyChanged {
-        private DvdFolderToImport folderToImport;
+        public DvdFolderToImport FolderToImport;
         public string FolderPath {
-            get { return folderToImport.FolderPath; } 
+            get { return FolderToImport.FolderPath; } 
             set {
-                folderToImport = new DvdFolderToImport(FolderPath);
-                folderToImport.GetFiles();
+                FolderToImport.FolderPath = value;
+                FolderToImport.GetFiles();
                 OnPropertyChange("Files");
+
             } 
         }
 
-        public List<FileToImport> Files {
-            get { return folderToImport.Files; }
+        public string FolderName {
+            get { return Path.GetFileName(FolderPath); }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public List<FileToImport> Files {
+            get; set;
+        }
+
+        public FolderToImportViewModel() {
+            string pathToGetFromSettingsFile = @"C:\temp\Transfer to HD\keepinganonymous1";
+            FolderToImport = new DvdFolderToImport(pathToGetFromSettingsFile);
+            Files = FolderToImport.Files;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
         protected void OnPropertyChange(string propertyName) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
--- DvdImportClient/FoldersToImport/FolderToImportView.xaml.cs
+++ DvdImportClient/FoldersToImport/FolderToImportView.xaml.cs
@@ -13,13 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DvdImportClient {
+namespace DvdImportClient.FoldersToImport {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FolderToImportView.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class FolderToImportView : UserControl {
+        public FolderToImportView() {
             InitializeComponent();
+            DataContext = new FolderToImportViewModel();
+
         }
     }
 }
