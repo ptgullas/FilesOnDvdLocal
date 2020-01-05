1fa845fd5543041f8fa16fe32aac06460558863c
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-05 00:04:01 -0500

    Implemented GetPerformers (returns hardcoded list of TV characters). Modified GetPerformerByName so it works closer to expected.

--- FilesOnDvdLocal/AccessMockRepository.cs
+++ FilesOnDvdLocal/AccessMockRepository.cs
@@ -7,12 +7,18 @@ using FilesOnDvdLocal.LocalDbDtos;
 
 namespace FilesOnDvdLocal {
     public class AccessMockRepository : IDataRepository {
+
+        private List<PerformerLocalDto> performers;
+
+        public AccessMockRepository() {
+            performers = GetMockPerformers();
+        }
+
         public PerformerLocalDto GetPerformerByName(string performerName) {
-            if (performerName.Last().ToString().ToLower() == "i") {
-                return new PerformerLocalDto() {
-                    Id = 9999,
-                    Name = performerName
-                };
+            var performer = performers
+                .FirstOrDefault(p => p.Name.ToLower() == performerName.ToLower());
+            if (performer != null) {
+                return performer;
             }
             else {
                 return new PerformerLocalDto() {
@@ -23,7 +29,30 @@ namespace FilesOnDvdLocal {
         }
 
         public List<PerformerLocalDto> GetPerformers() {
-            throw new NotImplementedException();
+            return performers.OrderBy(p => p.Name).ToList();
+        }
+
+        private List<PerformerLocalDto> GetMockPerformers() {
+            List<PerformerLocalDto> mockPerformers = new List<PerformerLocalDto>() {
+                new PerformerLocalDto() {Id = 1, Name = "Shiv Roy"},
+                new PerformerLocalDto() {Id = 2, Name = "Roman Roy"},
+                new PerformerLocalDto() {Id = 3, Name = "Kendall Roy"},
+                new PerformerLocalDto() {Id = 4, Name = "Tandy Bowen"},
+                new PerformerLocalDto() {Id = 5, Name = "Tyrone Johnson"},
+                new PerformerLocalDto() {Id = 6, Name = "The Doctor"},
+                new PerformerLocalDto() {Id = 7, Name = "Jeff Winger"},
+                new PerformerLocalDto() {Id = 8, Name = "Annie Edison"},
+                new PerformerLocalDto() {Id = 9, Name = "Britta Perry"},
+                new PerformerLocalDto() {Id = 10, Name = "Matt Murdock"},
+                new PerformerLocalDto() {Id = 11, Name = "Archie Andrews"},
+                new PerformerLocalDto() {Id = 12, Name = "Toni Topaz"},
+                new PerformerLocalDto() {Id = 13, Name = "Baby Yoda"},
+                new PerformerLocalDto() {Id = 14, Name = "The Mandalorian"},
+                new PerformerLocalDto() {Id = 15, Name = "Jon Osterman"},
+                new PerformerLocalDto() {Id = 16, Name = "Laurie Blake"},
+                new PerformerLocalDto() {Id = 17, Name = "Angela Abar"},
+            };
+            return mockPerformers;
         }
     }
 }
