7fa0cf4ac126f699396726e1d4e0ce2dd35fd4a6
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-01-06 17:26:19 -0500

    Added AccessRepositoryTests

---
+++ FilesOnDvdLocal.Tests/AccessRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FilesOnDvdLocal.LocalDbDtos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests
+{
+    [TestClass]
+    public class AccessRepositoryTests
+    {
+        [TestMethod]
+        public void GetPerformers_GetActualPerformers_ReturnsPerformers() {
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            AccessRepository repository = new AccessRepository(dbLocation);
+            PerformerLocalDto expected = new PerformerLocalDto() {
+                Id = 177,
+                Name = "Steve Austin"
+            };
+
+            List<PerformerLocalDto> performersInDb = repository.GetPerformers();
+
+            var result = performersInDb.FirstOrDefault(p => p.Name.ToLower() == "steve austin");
+            Assert.AreEqual(expected.Id, result.Id);
+        }
+    }
+}
