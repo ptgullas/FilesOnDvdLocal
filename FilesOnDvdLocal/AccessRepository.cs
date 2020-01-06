81b9fb85f047fcce94624885d4c8fe143e9615de
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-01-06 18:09:06 -0500

    Added AddDisc & GetDiscIdByName. Added try-catch to RetrievePerformers

--- FilesOnDvdLocal/AccessRepository.cs
+++ FilesOnDvdLocal/AccessRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FilesOnDvdLocal.Data;
 using FilesOnDvdLocal.LocalDbDtos;
+using Serilog;
 
 namespace FilesOnDvdLocal {
     public class AccessRepository : IDataRepository {
@@ -23,6 +24,38 @@ namespace FilesOnDvdLocal {
             return Performers;
         }
 
+        public void AddDisc(DvdFolderToImport dvd) {
+            AccessRetriever retriever = new AccessRetriever(DatabasePath);
+            try {
+                DataSet dataSet = retriever.GetDiscs();
+                DataRow newDisc = dataSet.Tables[0].NewRow();
+                newDisc["DiscName"] = dvd.DiscName;
+                newDisc["Wallet"] = dvd.WalletType;
+                newDisc["Notes"] = dvd.Notes;
+                dataSet.Tables[0].Rows.Add(newDisc);
+                retriever.UpdateDiscs(dataSet);
+            }
+            catch (Exception e) {
+                Log.Error(e, "Error adding disc {0} to database", dvd.DiscName);
+            }
+        }
+
+        public int GetDiscIdByName(string discName) {
+            AccessRetriever retriever = new AccessRetriever(DatabasePath);
+            int discId = -1;
+            try {
+                DataSet dataSet = retriever.GetDiscs();
+                DataTable discsTable = dataSet.Tables[0];
+                DataRow[] selectedDiscsRows = discsTable.Select($"DiscName = '{discName}'");
+                string discIdStr = selectedDiscsRows[0]["ID"].ToString();
+                bool isInt = int.TryParse(discIdStr, out discId);
+            }
+            catch (Exception e) {
+                Log.Error(e, "Could not retrieve Disc {0}", discName);
+            }
+            return discId;
+        }
+
         public PerformerLocalDto GetPerformerByName(string performerName) {
             PerformerLocalDto perf = Performers.FirstOrDefault(b =>
                 b.Name.ToUpper() == performerName.ToUpper());
@@ -37,17 +70,23 @@ namespace FilesOnDvdLocal {
         }
 
         private void RetrievePerformers() {
-            AccessRetriever retriever = new AccessRetriever(DatabasePath);
-            DataSet resultsetFromDb = retriever.GetPerformers();
-            DataTable performersTable = resultsetFromDb.Tables[0];
-            foreach (DataRow row in performersTable.Rows) {
-                string idStr = row["ID"].ToString();
-                bool result = int.TryParse(idStr, out int id);
-                PerformerLocalDto performer = new PerformerLocalDto() {
-                    Id = id,
-                    Name = row["Performer"].ToString()
-                };
-                Performers.Add(performer);
+            try {
+                AccessRetriever retriever = new AccessRetriever(DatabasePath);
+                DataSet resultsetFromDb = retriever.GetPerformers();
+                DataTable performersTable = resultsetFromDb.Tables[0];
+                foreach (DataRow row in performersTable.Rows) {
+                    string idStr = row["ID"].ToString();
+                    bool result = int.TryParse(idStr, out int id);
+                    PerformerLocalDto performer = new PerformerLocalDto() {
+                        Id = id,
+                        Name = row["Performer"].ToString()
+                    };
+                    Performers.Add(performer);
+                }
+            }
+            catch (Exception e) {
+                Log.Error(e, "Could not retrieve performers from database");
+                throw new ArgumentException("Could not retrieve performers from database", e);
             }
         }
     }
