dd91d7db0009c435ea6e22e703e31036f92acc0e
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-12 14:24:16 -0500

    Added Serilog to WPF project (doesn't fully work yet)

--- DvdImportClient/App.xaml.cs
+++ DvdImportClient/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,5 +12,29 @@ namespace DvdImportClient {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        void OnApplicationStartup(object sender, StartupEventArgs se) {
+            SetUpLogging();
+
+
+        }
+
+        private static void SetUpLogging() {
+            try {
+                //string logFolder = Configuration.GetSection("LocalFolders").GetValue<string>("logFolder");
+                //string logFile = Configuration.GetSection("LocalFolders").GetValue<string>("logFileName");
+                //string logPath = Path.Combine(logFolder, logFile);
+                string logPath = @"c:\Logs\FilesOnDvdLocal\FilesOnDvdLocalLog.txt";
+
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .WriteTo.File(logPath, rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Could not set up logging");
+                Console.WriteLine(e.GetBaseException());
+            }
+        }
+
     }
 }
