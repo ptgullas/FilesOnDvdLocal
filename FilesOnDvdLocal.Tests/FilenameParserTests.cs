6b91c49695a43267f5166fa880130410b19ec183
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-19 22:10:15 -0500

    Added FilenameParser to take some of that code out of FileToImport

---
+++ FilesOnDvdLocal.Tests/FilenameParserTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests {
+    [TestClass]
+    public class FilenameParserTests {
+        [TestMethod]
+        public void GetSeriesName_FilenameHasSeries_ReturnsSeriesName() {
+            string filename = @"Watchmen - Jean Smart, Jeremy Irons, Regina King - Little Fear of Lightning (2019-12-05).mkv";
+            string expected = "Watchmen";
+
+            string result = FilenameParser.GetSeriesName(filename);
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ FilesOnDvdLocal/FilenameParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal {
+    public static class FilenameParser {
+        public static string GetSeriesName(string filename) {
+            string seriesName = null;
+            int indexOfHyphen = filename.IndexOf("-");
+            if (indexOfHyphen >= 2) {
+                seriesName = filename.Substring(0, indexOfHyphen - 1).Trim();
+            }
+            return seriesName;
+        }
+    }
+}
