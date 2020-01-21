3a22b7a58a3c6a7c58d8b6693dacca3587c17585
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-20 23:39:06 -0500

    Added Importer class

---
+++ FilesOnDvdLocal/Importer.cs
@@ -0,0 +1,22 @@
+﻿using FilesOnDvdLocal.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal {
+    public class Importer {
+        private IDiscRepository discRepository;
+        private IPerformerRepository performerRepository;
+
+        public Importer(IDiscRepository discRepository, IPerformerRepository performerRepository) {
+            this.discRepository = discRepository;
+            this.performerRepository = performerRepository;
+        }
+
+        public void Import(DvdFolderToImport dvdFolder) {
+            discRepository.Add(dvdFolder);
+        }
+    }
+}
