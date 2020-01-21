ee31a2ed67e551f032a04a45c991028cf4343ffa
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-20 20:58:44 -0500

    Added IDiscRepository, DiscMockRepository &  Newtonsoft package

---
+++ FilesOnDvdLocal/Repositories/DiscMockRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.Repositories {
+    public class DiscMockRepository : IDiscRepository {
+        public void Add(DvdFolderToImport disc) {
+            throw new NotImplementedException();
+        }
+    }
+}
