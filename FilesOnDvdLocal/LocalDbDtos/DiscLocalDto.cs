7803f1e58b0291b22d967b987ae6f256d110aab5
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-20 23:37:46 -0500

    Added Notes field, and constructors.

--- FilesOnDvdLocal/LocalDbDtos/DiscLocalDto.cs
+++ FilesOnDvdLocal/LocalDbDtos/DiscLocalDto.cs
@@ -9,5 +9,16 @@ namespace FilesOnDvdLocal.LocalDbDtos {
         public int Id { get; set; }
         public string DiscName { get; set; }
         public int Wallet { get; set; }
+        public string Notes { get; set; }
+
+        public DiscLocalDto() {
+
+        }
+
+        public DiscLocalDto(DvdFolderToImport dvdFolder) {
+            DiscName = dvdFolder.DiscName;
+            Wallet = dvdFolder.WalletType;
+            Notes = dvdFolder.Notes;
+        }
     }
 }
