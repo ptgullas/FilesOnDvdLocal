f120543f00eeecbcc736c63f194e5dabcd9bf13a
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-20 23:38:40 -0500

    Added basic methods

--- FilesOnDvdLocal/Repositories/DiscMockRepository.cs
+++ FilesOnDvdLocal/Repositories/DiscMockRepository.cs
@@ -1,13 +1,73 @@
-﻿using System;
+﻿using FilesOnDvdLocal.LocalDbDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Serilog;
 
 namespace FilesOnDvdLocal.Repositories {
     public class DiscMockRepository : IDiscRepository {
+
+        private List<DiscLocalDto> Discs;
+        private readonly string PathToJson;
+
+        public DiscMockRepository(string pathToJson) {
+            Discs = new List<DiscLocalDto>();
+            PathToJson = pathToJson;
+        }
         public void Add(DvdFolderToImport disc) {
-            throw new NotImplementedException();
+            AddDiscToList(disc);
+            string jsonDiscs = SerializeDiscs();
+            SaveToFile(jsonDiscs, PathToJson);
+        }
+
+        // for testing
+        public void Add(DiscLocalDto disc) {
+            Discs.Add(disc);
+        }
+        // for testing
+        public void Save() {
+            string jsonDiscs = SerializeDiscs();
+            SaveToFile(jsonDiscs, PathToJson);
+        }
+
+        public void RetrieveDiscs() {
+            if (File.Exists(PathToJson)) {
+                string jsonContents = File.ReadAllText(PathToJson);
+                Discs = JsonConvert.DeserializeObject<List<DiscLocalDto>>(jsonContents);
+            }
+            else {
+                throw new FileNotFoundException("Can't find TVDBInfo file", PathToJson);
+            }
+
+        }
+
+        private void AddDiscToList(DvdFolderToImport disc) {
+            DiscLocalDto discDto = new DiscLocalDto(disc);
+            if (!Discs.Any(d => d.DiscName == discDto.DiscName)) {
+                int highestId = Discs.Max(d => d.Id);
+                discDto.Id = highestId + 1;
+                Discs.Add(discDto);
+            }
+            else {
+                throw new ArgumentOutOfRangeException(disc.DiscName, "Disc name already exists in Database");
+            }
+        }
+
+        private string SerializeDiscs() {
+            return JsonConvert.SerializeObject(Discs, Formatting.Indented);
+        }
+
+        private void SaveToFile(string jsonContents, string filePath) {
+            try {
+                File.WriteAllText(filePath, jsonContents);
+            }
+            catch(Exception e) {
+                Log.Error(e, "Could not save Discs json to file {0}", filePath);
+            }
         }
     }
 }
