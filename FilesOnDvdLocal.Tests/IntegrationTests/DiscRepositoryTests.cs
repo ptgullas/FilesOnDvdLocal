2d02d03dfc437a0efc9973b33239c96c5aa69848
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-01-21 16:11:02 -0500

    Added DiscRepository & tests

---
+++ FilesOnDvdLocal.Tests/IntegrationTests/DiscRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FilesOnDvdLocal.LocalDbDtos;
+using FilesOnDvdLocal.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests.IntegrationTests
+{
+    [TestClass]
+    public class DiscRepositoryTests
+    {
+        [TestMethod]
+        public void Add_NewDisc_AddsToDiscTable() {
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            DiscRepository repository = new DiscRepository(dbLocation);
+            DiscLocalDto discDto = new DiscLocalDto() { DiscName = "TestingDisc", Wallet = 3, Notes = "A Test from .NET" };
+
+            repository.Add(discDto);
+
+            Console.WriteLine("Debug this line");
+
+        }
+    }
+}
