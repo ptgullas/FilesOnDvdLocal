d52eee3b75032ea3a83fd92b3ff8885b6e380462
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-20 21:03:16 -0500

    Added DiscLocalDto

---
+++ FilesOnDvdLocal/LocalDbDtos/DiscLocalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.LocalDbDtos {
+    public class DiscLocalDto {
+        public int Id { get; set; }
+        public string DiscName { get; set; }
+        public int Wallet { get; set; }
+    }
+}
