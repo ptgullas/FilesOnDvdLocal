bbeaa403ddaa3db671006ceeed41eeb70dbb7b4b
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-23 23:58:00 -0500

    Added OperationResult class (from D. Kurata's Defensive Coding course).

---
+++ FilesOnDvdLocal/OperationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal {
+    public class OperationResult {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
