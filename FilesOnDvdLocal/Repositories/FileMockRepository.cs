b443d186a0406b708e4567cafbf6707533970e74
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-26 23:50:25 -0500

    fleshed out FileMockRepository (similar to DiscMockRepository)

--- FilesOnDvdLocal/Repositories/FileMockRepository.cs
+++ FilesOnDvdLocal/Repositories/FileMockRepository.cs
@@ -1,23 +1,76 @@
 ﻿using FilesOnDvdLocal.LocalDbDtos;
+using Newtonsoft.Json;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilesOnDvdLocal.Repositories {
     public class FileMockRepository : IFileRepository {
+
+        private List<FileLocalDto> files;
+        private readonly string pathToJson;
+
+        public FileMockRepository(string pathToJson) {
+            this.pathToJson = pathToJson;
+            files = new List<FileLocalDto>();
+        }
+
         public int Add(FileToImport file) {
             FileLocalDto fileDto = new FileLocalDto(file);
             return Add(fileDto);
         }
 
         public int Add(FileLocalDto fileDto) {
-            throw new NotImplementedException();
+            int newId = AddFileToList(fileDto); 
+            string jsonFiles = SerializeFilenames();
+            SaveToFile(jsonFiles, pathToJson);
+            return newId;
         }
+
+
+        public int AddFileToList(FileLocalDto fileDto) {
+            int highestId = 0;
+            if (files.Any()) {
+                highestId = (int) files.Max(f => f.Id);
+                highestId++;
+            }
+            fileDto.Id = highestId;
+            files.Add(fileDto);
+            return highestId;
+        }
+
+        private string SerializeFilenames() {
+            return JsonConvert.SerializeObject(files, Formatting.Indented);
+
+        }
+
+        private void SaveToFile(string jsonContents, string filePath) {
+            try {
+                File.WriteAllText(filePath, jsonContents);
+            }
+            catch (Exception e) {
+                Log.Error(e, "Could not save Filenames json to file {0}", filePath);
+            }
+        }
+
+        public void RetrieveFiles() {
+            if (File.Exists(pathToJson)) {
+                string jsonContents = File.ReadAllText(pathToJson);
+                files = JsonConvert.DeserializeObject<List<FileLocalDto>>(jsonContents);
+            }
+            else {
+                throw new FileNotFoundException("Can't find json file", pathToJson);
+            }
+        }
+
 
         public List<FileLocalDto> GetByDisc(int discId) {
             throw new NotImplementedException();
         }
+
     }
 }
