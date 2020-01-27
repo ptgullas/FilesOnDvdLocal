53f84c9327fa819facb2afaad7fb0705e103aed8
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-01-27 15:08:55 -0500

    Added PerformerFilenameJoinDto

---
+++ FilesOnDvdLocal/LocalDbDtos/PerformerFilenameJoinDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.LocalDbDtos
+{
+    public class PerformerFilenameJoinDto {
+        public int PerformerId { get; set; }
+        public int FilenameId { get; set; }
+    }
+}
