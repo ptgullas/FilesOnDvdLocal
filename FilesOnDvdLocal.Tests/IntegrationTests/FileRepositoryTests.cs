be323921e5fbc2fcaf346d6db4397eb6fdc69e75
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-01-28 15:37:53 -0500

    Added FileRepository + test class

---
+++ FilesOnDvdLocal.Tests/IntegrationTests/FileRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FilesOnDvdLocal.LocalDbDtos;
+using FilesOnDvdLocal.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilesOnDvdLocal.Tests.IntegrationTests
+{
+    [TestClass]
+    public class FileRepositoryTests
+    {
+        [TestMethod]
+        public void Add_AddFileDtosList_AddsToDb() {
+            string dbLocation = @"c:\temp\Files on Dvd.accdb";
+            FileRepository repository = new FileRepository(dbLocation);
+            List<FileLocalDto> fileDtos = new List<FileLocalDto>() {
+                new FileLocalDto() {Disc = 225, Filename = "2020-01-27 WWE RAW.mkv", Genre = 4, Series = 88 },
+                new FileLocalDto() {Disc = 225, Filename = "2020-01-22 WWE NXT.mkv", Genre = 4, Series = 70 },
+                new FileLocalDto() {Disc = 225, Filename = "2020-01-26 WWE Royal Rumble 2020.mp4", Genre = 4},
+                new FileLocalDto() {Disc = 225, Filename = "2020-01-22 AEW Dynamite.avi", Genre = 4},
+            };
+            repository.Add(fileDtos);
+
+            Console.WriteLine("Debug this line");
+        
+        }
+    }
+}
---
+++ FilesOnDvdLocal/Repositories/FileRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FilesOnDvdLocal.Data;
+using FilesOnDvdLocal.LocalDbDtos;
+using Serilog;
+
+namespace FilesOnDvdLocal.Repositories
+{
+    public class FileRepository : IFileRepository
+    {
+        private string databasePath;
+
+        public FileRepository(string databasePath) {
+            this.databasePath = databasePath;
+        }
+
+        public int Add(FileToImport file) {
+            throw new NotImplementedException();
+        }
+
+        public int Add(FileLocalDto fileDto) {
+            throw new NotImplementedException();
+        }
+
+        public void Add(List<FileToImport> files) {
+            if (files != null && files.Count > 0) {
+                List<FileLocalDto> fileDtos = new List<FileLocalDto>();
+                foreach (FileToImport file in files) {
+                    FileLocalDto fileDto = new FileLocalDto(file);
+                    fileDtos.Add(fileDto);
+                }
+                Add(fileDtos);
+            }
+        }
+
+        public void Add(List<FileLocalDto> fileDtos) {
+            AccessRetriever retriever = new AccessRetriever(databasePath);
+
+            DataSet dataSet = RetrieveFileEntriesFromDatabase(retriever);
+            DataTable filenamesTable = dataSet.Tables[0];
+            foreach (FileLocalDto file in fileDtos) {
+                DataRow newRow = filenamesTable.NewRow();
+                newRow["Filename"] = file.Filename;
+                newRow["Genre"] = file.Genre;
+                newRow["Disc"] = file.Disc;
+                if (file.Series is null) {
+                    newRow["Series"] = DBNull.Value;
+                }
+                else {
+                    newRow["Series"] = file.Series;
+                }
+                filenamesTable.Rows.Add(newRow);
+            }
+            retriever.UpdateFileEntries(dataSet);
+        }
+
+        private DataSet RetrieveFileEntriesFromDatabase(AccessRetriever retriever) {
+            DataSet dataSet;
+            try {
+                dataSet = retriever.GetFileEntries();
+            }
+            catch (Exception e) {
+                Log.Error(e, "Could not retrieve filenames from Access DB");
+                throw new InvalidOperationException("Could not retrieve filenames from Access DB", e);
+            }
+            if (dataSet is null) { throw new InvalidOperationException("Filenames dataSet is null"); }
+            return dataSet;
+        }
+
+        public List<FileLocalDto> GetByDisc(int discId) {
+            throw new NotImplementedException();
+        }
+    }
+}
