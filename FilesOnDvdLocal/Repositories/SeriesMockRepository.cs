d7013f98784663dbce11d40de038daad40b5b082
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-27 22:06:42 -0500

    Added SeriesMockRepository

---
+++ FilesOnDvdLocal/Repositories/SeriesMockRepository.cs
@@ -0,0 +1,22 @@
+﻿using FilesOnDvdLocal.LocalDbDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.Repositories {
+    public class SeriesMockRepository : ISeriesRepository {
+
+        private List<SeriesLocalDto> series;
+        private string pathToJson;
+
+        public List<SeriesLocalDto> Get() {
+            throw new NotImplementedException();
+        }
+
+        public SeriesLocalDto Get(string name) {
+            throw new NotImplementedException();
+        }
+    }
+}
