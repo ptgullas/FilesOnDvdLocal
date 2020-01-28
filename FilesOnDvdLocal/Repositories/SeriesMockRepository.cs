3a95dd8e9a6a99da160942ad1975bafab0aea74a
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-28 09:31:01 -0500

    Implemented constructor & Get methods

--- FilesOnDvdLocal/Repositories/SeriesMockRepository.cs
+++ FilesOnDvdLocal/Repositories/SeriesMockRepository.cs
@@ -1,6 +1,8 @@
 ﻿using FilesOnDvdLocal.LocalDbDtos;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,29 @@ namespace FilesOnDvdLocal.Repositories {
         private List<SeriesLocalDto> series;
         private string pathToJson;
 
+        public SeriesMockRepository(string pathToJson) {
+            series = new List<SeriesLocalDto>();
+            this.pathToJson = pathToJson;
+            RetrieveSeries();
+        }
+
+        private void RetrieveSeries() {
+            if (File.Exists(pathToJson)) {
+                string jsonContents = File.ReadAllText(pathToJson);
+                series = JsonConvert.DeserializeObject<List<SeriesLocalDto>>(jsonContents);
+            }
+            else {
+                throw new FileNotFoundException("Can't find json file", pathToJson);
+            }
+        }
+
+
         public List<SeriesLocalDto> Get() {
-            throw new NotImplementedException();
+            return series;
         }
 
         public SeriesLocalDto Get(string name) {
-            throw new NotImplementedException();
+            return series.FirstOrDefault(s => s.Name == name);
         }
     }
 }
