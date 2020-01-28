e625e74cfb04f08a59c27c84e69610dee5ce0481
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-28 09:10:31 -0500

    Added Add(List<FileToImport>)

--- FilesOnDvdLocal/Repositories/IFileRepository.cs
+++ FilesOnDvdLocal/Repositories/IFileRepository.cs
@@ -9,6 +9,7 @@ namespace FilesOnDvdLocal.Repositories {
     public interface IFileRepository {
         int Add(FileToImport file);
         int Add(FileLocalDto fileDto);
+        void Add(List<FileToImport> files);
         List<FileLocalDto> GetByDisc(int discId);
     }
 }
