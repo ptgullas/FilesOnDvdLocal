a38d2185ebb3df9452bc4557e58d643cced2c523
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-31 08:04:43 -0500

    Added RepositoryFactory

---
+++ FilesOnDvdLocal/RepositoryFactory.cs
@@ -0,0 +1,31 @@
+﻿using FilesOnDvdLocal.Options;
+using FilesOnDvdLocal.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal {
+    public class RepositoryFactory {
+        private LocalPathOptions localPathOptions;
+        private bool useMockRepositories;
+        public RepositoryFactory(LocalPathOptions localPathOptions, bool useMockRepositories) {
+            this.localPathOptions = localPathOptions;
+            this.useMockRepositories = useMockRepositories;
+        }
+
+        public IFileRepository GetFileRepository() {
+            if (useMockRepositories) { return CreateFileMockRepository(); }
+            else { return CreateFileRepository(); }
+        }
+
+        private FileMockRepository CreateFileMockRepository() {
+            return new FileMockRepository(localPathOptions.FileMockRepositoryPath);
+        }
+
+        private FileRepository CreateFileRepository() {
+            return new FileRepository(localPathOptions.DatabasePath);
+        }
+    }
+}
