918a72058ea71864005313f40b1a722b3941c959
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-31 10:57:38 -0500

    Added JoinsMockRepositoryPath

--- FilesOnDvdLocal/Options/LocalPathOptions.cs
+++ FilesOnDvdLocal/Options/LocalPathOptions.cs
@@ -12,6 +12,7 @@ namespace FilesOnDvdLocal.Options {
         public string PerformerMockRepositoryPath { get; set; }
         public string DiscMockRepositoryPath { get; set; }
         public string SeriesMockRepositoryPath { get; set; }
+        public string JoinsMockRepositoryPath { get; set; }
         public string FileListingFolder { get; set; }
         public string DatabasePath { get; set; }
     }
