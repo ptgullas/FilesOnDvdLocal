4fbba3a9b4deb588bbe88969d12df5e62487a4b6
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-31 07:47:55 -0500

    Renamed to LocalPathOptions, added more properties

---
+++ FilesOnDvdLocal/Options/LocalPathOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.Options {
+    public class LocalPathOptions {
+        public string DvdToImportStartFolder { get; set; }
+        public string MockRepositoryFolder { get; set; }
+        public string FileMockRepositoryPath { get; set; }
+        public string DiscMockRepositoryPath { get; set; }
+        public string SeriesMockRepositoryPath { get; set; }
+        public string FileListingFolder { get; set; }
+        public string DatabasePath { get; set; }
+    }
+}
