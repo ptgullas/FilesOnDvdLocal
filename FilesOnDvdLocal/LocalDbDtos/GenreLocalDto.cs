e9c000088184b192600b04aa682ccd717e4fdbd6
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-01-31 13:04:31 -0500

    Added GenreLocalDto

---
+++ FilesOnDvdLocal/LocalDbDtos/GenreLocalDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.LocalDbDtos {
+    public class GenreLocalDto {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+    }
+}
