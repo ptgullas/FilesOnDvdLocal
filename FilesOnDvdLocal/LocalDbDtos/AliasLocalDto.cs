dc068e51ed4be14e0eb4bd708cbc98acac94221e
Paul Gullas <4305689+ptgullas@users.noreply.github.com>
2020-02-07 14:43:26 -0500

    Added AliasLocalDto

---
+++ FilesOnDvdLocal/LocalDbDtos/AliasLocalDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesOnDvdLocal.LocalDbDtos
+{
+    public class AliasLocalDto {
+        public int Id { get; set; }
+        public int Performer { get; set; }
+        public string Alias { get; set; }
+    }
+}
