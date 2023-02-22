bcf80a2c3bc607f9102b75439f24d246cfc77df6
ptgullas <4305689+ptgullas@users.noreply.github.com>
2023-02-22 00:53:49 -0500

    add LegacyGenreService & LegacyWalletService

---
+++ MigrateLegacy.Services/LegacyGenreService.cs
@@ -0,0 +1,18 @@
+﻿using LegacyMediaFilesOnDvd.Data.Context;
+using LegacyMediaFilesOnDvd.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MigrateLegacy.Services {
+    public class LegacyGenreService {
+        private LegacyMediaFilesContext _legacyContext;
+
+        public LegacyGenreService(LegacyMediaFilesContext legacyContext) {
+            _legacyContext = legacyContext;
+        }
+
+        public IEnumerable<LegacyGenre> Get() {
+            return _legacyContext.LegacyGenres
+                .OrderBy(x => x.Id);
+        }
+    }
+}
---
+++ MigrateLegacy.Services/LegacyWalletService.cs
@@ -0,0 +1,18 @@
+﻿using LegacyMediaFilesOnDvd.Data.Context;
+using LegacyMediaFilesOnDvd.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MigrateLegacy.Services {
+    public class LegacyWalletService {
+        private LegacyMediaFilesContext _legacyContext;
+
+        public LegacyWalletService(LegacyMediaFilesContext legacyContext) {
+            _legacyContext = legacyContext;
+        }
+
+        public IEnumerable<LegacyWallet> Get() {
+            return _legacyContext.LegacyWallets
+                .OrderBy(lw => lw.Id);
+        }
+    }
+}
