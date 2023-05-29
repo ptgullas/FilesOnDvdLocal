1d1a2ba46c35232507f36301d0e342aa316994b2
ptgullas <4305689+ptgullas@users.noreply.github.com>
2023-05-29 16:23:42 -0400

    add LegacySeriesService & Tests

---
+++ MigrateLegacy.Services/LegacySeriesService.cs
@@ -0,0 +1,72 @@
+﻿using LegacyMediaFilesOnDvd.Data.Context;
+using LegacyMediaFilesOnDvd.Data.Models;
+using MediaFilesOnDvd.Data.Entities;
+using MediaFilesOnDvd.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrateLegacy.Services {
+    public class LegacySeriesService {
+        private LegacyMediaFilesContext _legacyContext;
+
+        public LegacySeriesService(LegacyMediaFilesContext legacyContext) {
+            _legacyContext = legacyContext;
+        }
+
+        public IEnumerable<LegacySeries> Get() {
+            return _legacyContext.LegacySeries
+                // leave out LegacyFilenames because we don't want to migrate them 
+                // at the same time as LegacySeries (which comes earlier)
+                // .Include(ls => ls.LegacyFilenames)
+                .Include (ls => ls.Publisher)
+                .Include (ls => ls.Genre)
+                .OrderBy(ls => ls.Id);
+        }
+
+        public IEnumerable<Series> MigrateToNewSeries(IEnumerable<LegacySeries> legacySeries, SeriesPublisherService seriesPublisherService, FileGenreService fileGenreService) {
+            List<Series> newSeries = new();
+            var fileGenres = fileGenreService.Get();
+            var seriesPublishers = seriesPublisherService.Get();
+            foreach (var legacySeriesItem in legacySeries) {
+                Series series = new() {
+                    Name = legacySeriesItem.Name
+                };
+
+                AddNewSeriesToNewGenre(fileGenres, legacySeriesItem, series);
+                AddNewSeriesToNewPublisher(seriesPublishers, legacySeriesItem, series);
+
+                newSeries.Add(series);
+            }
+
+            return newSeries;
+        }
+
+        private static void AddNewSeriesToNewPublisher(IEnumerable<SeriesPublisher> seriesPublishers, LegacySeries legacySeriesItem, Series series) {
+            if (legacySeriesItem.Publisher is not null) {
+                var newPublisher = seriesPublishers.FirstOrDefault(
+                    p => p.Name.ToLower() == legacySeriesItem.Publisher.Name.ToLower());
+                if (newPublisher is not null) {
+                    newPublisher.Series.Add(series);
+                }
+                else {
+                    throw new ArgumentException($"{legacySeriesItem.Publisher.Name} not found in NewPublishers.");
+                }
+            }
+        }
+
+        private static void AddNewSeriesToNewGenre(IEnumerable<FileGenre> fileGenres, LegacySeries legacySeriesItem, Series series) {
+            var newParentGenre = fileGenres.FirstOrDefault(
+                f => f.Name.ToLower() == legacySeriesItem.Genre.Name.ToLower());
+            if (newParentGenre is not null) {
+                newParentGenre.Series.Add(series);
+            }
+            else {
+                throw new ArgumentException($"{legacySeriesItem.Genre.Name} not found in fileGenres.");
+            }
+        }
+    }
+}
---
+++ MigrateLegacy.Tests/LegacySeriesServiceTests.cs
@@ -0,0 +1,143 @@
+﻿using LegacyMediaFilesOnDvd.Data.Context;
+using LegacyMediaFilesOnDvd.Data.Models;
+using MediaFilesOnDvd.Data;
+using MediaFilesOnDvd.Data.Entities;
+using MediaFilesOnDvd.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MigrateLegacy.Services;
+using System.Data.Common;
+
+namespace MigrateLegacy.Tests {
+    public class LegacySeriesServiceTests {
+        const string pathToLegacyDb = @"Data Source=C:\temp\FilesOnDvd\MigrationToSqlLite\LegacyMediaFilesOnDvd.db";
+
+        private readonly DbConnection _modernConnection;
+        private readonly DbContextOptions<MediaFilesContext> _modernContextOptions;
+
+        #region ConstructorAndDispose
+
+        public LegacySeriesServiceTests() {
+            _modernConnection = new SqliteConnection("Filename=:memory:");
+            _modernConnection.Open();
+
+            _modernContextOptions = new DbContextOptionsBuilder<MediaFilesContext>()
+                .UseSqlite(_modernConnection)
+                .Options;
+            // create the schema & seed some data
+            using var modernContext = new MediaFilesContext(_modernContextOptions);
+            if (modernContext.Database.EnsureCreated()) {
+            }
+
+            var genres = new FileGenre[] {
+                new FileGenre { Name = "Music" },
+                new FileGenre { Name = "Music Video" },
+                new FileGenre { Name = "Porn" },
+                new FileGenre { Name = "Wrestling" },
+                new FileGenre { Name = "TV Show" },
+                new FileGenre { Name = "Film" },
+                new FileGenre { Name = "Videoclip" },
+                new FileGenre { Name = "Podcast" },
+                new FileGenre { Name = "MMA" },
+                new FileGenre { Name = "Shoot interview" },
+                new FileGenre { Name = "Boxing" },
+                new FileGenre { Name = "Web series" }
+            };
+            var publishers = new SeriesPublisher[] {
+                new SeriesPublisher { Name = "Brazzers" },
+                new SeriesPublisher { Name = "RealityKings" },
+                new SeriesPublisher { Name = "Elegant Angel" },
+                new SeriesPublisher { Name = "Naughty America" },
+                new SeriesPublisher { Name = "Bangbros" },
+                new SeriesPublisher { Name = "World Wrestling Entertainment" },
+                new SeriesPublisher { Name = "Impact Wrestling" },
+                new SeriesPublisher { Name = "F4WOnline.com" },
+                new SeriesPublisher { Name = "Geek & Sundry" },
+                new SeriesPublisher { Name = "Mofos" },
+                new SeriesPublisher { Name = "Nuru" },
+                new SeriesPublisher { Name = "Combat Zone" },
+                new SeriesPublisher { Name = "CumLouder" },
+                new SeriesPublisher { Name = "DDF" },
+                new SeriesPublisher { Name = "TeamSkeet" },
+                new SeriesPublisher { Name = "Twistys" }
+            };
+
+            modernContext.FileGenres.AddRange(genres);
+            modernContext.SeriesPublishers.AddRange(publishers);
+            modernContext.SaveChanges();
+        }
+
+        public void Dispose() => _modernConnection.Dispose();
+
+        #endregion
+
+        [Fact]
+        public void Get_ReturnsAllSeries() {
+            // Arrange
+            var options = new DbContextOptionsBuilder<LegacyMediaFilesContext>()
+                .UseSqlite(pathToLegacyDb)
+                .Options;
+            var context = new LegacyMediaFilesContext(options);
+
+            LegacySeriesService legacySeriesService = new(context);
+
+            int seriesIdToRetrieve = 45;
+            int expectedSeriesCount = 137;
+            string expectedSeriesName = "Wrestling Observer Live";
+            string expectedPublisherName = "F4WOnline.com";
+            string expectedGenreName = "Podcast";
+            // Act
+
+            var results = legacySeriesService.Get();
+
+            var singleResult = results.First(i => i.Id == seriesIdToRetrieve);
+
+            // Assert
+
+            Assert.Equal(expectedSeriesCount, results.Count());
+            Assert.Equal(expectedSeriesName, singleResult.Name);
+            Assert.Equal(expectedPublisherName, singleResult.Publisher.Name);
+            Assert.Equal(expectedGenreName, singleResult.Genre.Name);
+        }
+
+        [Fact]
+        public void MigrateToNewSeries_CreatesNewSeriesItems() {
+            // Arrange
+            var options = new DbContextOptionsBuilder<LegacyMediaFilesContext>()
+                .UseSqlite(pathToLegacyDb)
+                .Options;
+            var context = new LegacyMediaFilesContext(options);
+
+            LegacySeriesService legacySeriesService = new(context);
+
+            using var modernContext = new MediaFilesContext(_modernContextOptions);
+            FileGenreService fileGenreService = new(modernContext);
+            SeriesPublisherService seriesPublisherService = new(modernContext);
+
+            int expectedNewSeriesCount = 137;
+            string seriesNameToRetrieve = "Wrestling Observer Live";
+            string expectedPublisherName = "F4WOnline.com";
+            string expectedGenreName = "Podcast";
+            // Act
+
+            var legacySeriesItems = legacySeriesService.Get();
+            var newSeriesItems = legacySeriesService.MigrateToNewSeries(legacySeriesItems, seriesPublisherService, fileGenreService);
+
+            modernContext.Series.AddRange(newSeriesItems);
+            modernContext.SaveChanges();
+
+            // Assert
+            SeriesService seriesService = new(modernContext);
+            var newSeriesFromService = seriesService.Get();
+
+            var newSeriesItem = newSeriesFromService.First(s => s.Name == seriesNameToRetrieve);
+
+            Assert.Equal(expectedNewSeriesCount, newSeriesFromService.Count());
+
+            Assert.Equal(expectedPublisherName, newSeriesItem.Publisher?.Name);
+            Assert.Equal(expectedGenreName, newSeriesItem.FileGenre.Name);
+
+        }
+    }
+}
